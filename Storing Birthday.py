ca384beb395da36a991b48ffc5a0fef38e009bca
Supriyo Mandal <supriyo.mandal27@gmail.com>
2024-09-09 18:13:01 +0530

    ACP

---
+++ Storing Birthday.py
@@ -0,0 +1,15 @@
+a=input('Enter the birthday of Arjun') 
+print("Arjun's birthday is on",a)
+b='25/06/2012'
+print("\nAveer's birthday is",b)
+c=input('Enter the birthday of Rehaan') 
+print('Rehaan\'s birthday is on  -',c)
+d=input('Enter the birthday of Ranvijay')
+print('Ranvijay\'s birthday is on  -',d)
+e='07/09/2012'
+print("\nVihaan's birthday is",e)
+
+
+
+
+
